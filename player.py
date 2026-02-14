b8f346a9b8d81ce08336461aec2310cfb559cc7f
KirillDemyanenko <demyanenkokg@gmail.com>
2026-02-14 15:52:57 +0300

    feat: npc

--- player.py
+++ player.py
@@ -24,8 +24,14 @@ class Player(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect(topleft=(x, y))
 
-        # Создаём маску из обрезанного изображения
-        self._update_mask()
+        # Маски для каждого кадра анимации (кэш)
+        self.mask_cache = {}
+        self.narrow_mask_cache = {}
+        self._update_masks()
+
+        # Хитбокс для коллизий без маски
+        self.hitbox = pygame.Rect(0, 0, 30, 64)
+        self.hitbox.center = self.rect.center
 
         self.width = self.rect.width
         self.height = self.rect.height
@@ -47,20 +53,16 @@ class Player(pygame.sprite.Sprite):
         self.projectiles = pygame.sprite.Group()
         self.attack_cooldown = 0
         self.is_attacking = False
-        self.attack_triggered = False  # чтобы выстрел был один за анимацию
+        self.attack_triggered = False
 
         # Частицы
         self.particles = []
-
-        # Для стабильности коллизий
-        self.ground_buffer = 2
 
     def _load_animations(self):
         """Загружает все анимации"""
         self.anim_manager.load_from_directory(self.assets_path, "idle", frame_duration=5, loop=True)
         self.anim_manager.load_from_directory(self.assets_path, "run", frame_duration=5, loop=True)
         self.anim_manager.load_from_directory(self.assets_path, "jump", frame_duration=5, loop=False)
-        # Атака: 12 кадров, каждый по 3 тика = 36 кадров total (0.6 сек при 60fps)
         self.anim_manager.load_from_directory(self.assets_path, "attack", frame_duration=3, loop=False)
 
         if not self.anim_manager.animations:
@@ -79,7 +81,6 @@ class Player(pygame.sprite.Sprite):
 
         for name, color in colors.items():
             frames = []
-            # Для атаки 12 кадров как в реальной анимации
             frame_count = 12 if name == "attack" else 4
             for i in range(frame_count):
                 surface = pygame.Surface((48 if name == "attack" else 32, 48), pygame.SRCALPHA)
@@ -87,11 +88,10 @@ class Player(pygame.sprite.Sprite):
                 width = 32
 
                 if name == "attack":
-                    # Анимация атаки: выдвигаем "оружие" постепенно и убираем
                     if i < 6:
-                        weapon_extend = i * 4  # 0, 4, 8, 12, 16, 20
+                        weapon_extend = i * 4
                     else:
-                        weapon_extend = (11 - i) * 4  # 20, 16, 12, 8, 4, 0
+                        weapon_extend = (11 - i) * 4
                     width = 32 + weapon_extend
                     pygame.draw.rect(surface, (150, 150, 150), (20, 20, 10 + weapon_extend, 8))
 
@@ -102,35 +102,73 @@ class Player(pygame.sprite.Sprite):
             loop = name not in ["attack", "jump"]
             self.anim_manager.add_animation(name, frames, frame_duration=3, loop=loop)
 
-    def _update_mask(self):
-        """Создаёт маску из обрезанного изображения"""
-        bbox = self.image.get_bounding_rect()
+    def _get_image_key(self, image):
+        """Создаёт уникальный ключ для кэширования маски"""
+        return id(image)
 
-        if bbox.width > 0 and bbox.height > 0:
-            trimmed = self.image.subsurface(bbox)
-            self.mask = pygame.mask.from_surface(trimmed)
-            self.mask_offset = (bbox.x, bbox.y)
-        else:
-            self.mask = pygame.mask.from_surface(self.image)
-            self.mask_offset = (0, 0)
+    def _create_narrow_mask(self, image):
+        """Создаёт узкую маску 30px шириной для X коллизий"""
+        bbox = image.get_bounding_rect()
+
+        if bbox.width <= 0 or bbox.height <= 0:
+            return pygame.mask.from_surface(image), 0, 0
+
+        body_width = min(30, bbox.width)
+        body_height = bbox.height
+
+        center_x = bbox.x + bbox.width // 2
+        left = center_x - body_width // 2
+        top = bbox.y
+
+        left = max(bbox.x, left)
+        if left + body_width > bbox.x + bbox.width:
+            body_width = bbox.x + bbox.width - left
+
+        if body_width <= 0 or body_height <= 0:
+            return pygame.mask.from_surface(image), 0, 0
+
+        narrow_rect = pygame.Rect(left, top, body_width, body_height)
+        narrow_surface = image.subsurface(narrow_rect)
+        narrow_mask = pygame.mask.from_surface(narrow_surface)
+
+        return narrow_mask, left, top
+
+    def _update_masks(self):
+        """Создаёт или берёт из кэша маски для текущего изображения"""
+        image_key = self._get_image_key(self.image)
+
+        # Полная маска (для Y коллизий)
+        if image_key not in self.mask_cache:
+            bbox = self.image.get_bounding_rect()
+
+            if bbox.width > 0 and bbox.height > 0:
+                trimmed = self.image.subsurface(bbox)
+                mask = pygame.mask.from_surface(trimmed)
+                offset_x = bbox.x
+                offset_y = bbox.y
+            else:
+                mask = pygame.mask.from_surface(self.image)
+                offset_x = 0
+                offset_y = 0
+
+            self.mask_cache[image_key] = (mask, offset_x, offset_y)
+
+        self.mask, self.mask_offset_x, self.mask_offset_y = self.mask_cache[image_key]
+
+        # Узкая маска 30px (для X коллизий)
+        if image_key not in self.narrow_mask_cache:
+            narrow_mask, narrow_offset_x, narrow_offset_y = self._create_narrow_mask(self.image)
+            self.narrow_mask_cache[image_key] = (narrow_mask, narrow_offset_x, narrow_offset_y)
+
+        self.narrow_mask, self.narrow_offset_x, self.narrow_offset_y = self.narrow_mask_cache[image_key]
+
+    def _update_hitbox(self):
+        """Обновляет хитбокс относительно rect"""
+        self.hitbox.centerx = self.rect.centerx
+        self.hitbox.bottom = self.rect.bottom - 10
 
     def update(self, platforms):
         keys = pygame.key.get_pressed()
-
-        # Управление движением (можно двигаться во время атаки, но не менять направление)
-        moving = False
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            if not self.is_attacking:  # Нельзя менять направление во время атаки
-                self.facing_right = False
-            self.velocity_x = -self.speed
-            moving = True
-        elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            if not self.is_attacking:
-                self.facing_right = True
-            self.velocity_x = self.speed
-            moving = True
-        else:
-            self.velocity_x *= self.friction
 
         # Атака по нажатию
         if self.attack_cooldown > 0:
@@ -140,13 +178,25 @@ class Player(pygame.sprite.Sprite):
             if self.attack_cooldown <= 0:
                 self._start_attack()
 
+        # Управление движением (нельзя двигаться во время атаки)
+        if not self.is_attacking:
+            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                self.facing_right = False
+                self.velocity_x = -self.speed
+            elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                self.facing_right = True
+                self.velocity_x = self.speed
+            else:
+                self.velocity_x *= self.friction
+        else:
+            self.velocity_x = 0
+
         # Проверяем окончание анимации атаки
         if self.is_attacking:
             current_anim = self.anim_manager.current_animation
             if current_anim and current_anim.finished:
                 self.is_attacking = False
                 self.attack_triggered = False
-                # Возвращаемся к подходящей анимации
                 if not self.on_ground:
                     self.anim_manager.play("jump")
                 elif abs(self.velocity_x) > 0.5:
@@ -164,39 +214,40 @@ class Player(pygame.sprite.Sprite):
         else:
             new_state = "idle"
 
-        # Меняем анимацию только если не атакуем или это принудительная смена
         if new_state != self.state:
             self.state = new_state
             if new_state == "attack":
                 self.anim_manager.play("attack", force_restart=True)
-            elif not self.is_attacking:  # Не переключаемся если атакуем
+            elif not self.is_attacking:
                 self.anim_manager.play(new_state)
 
-        # Прыжок (нельзя во время атаки)
+        # Прыжок
         if not self.is_attacking and (keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]) and self.on_ground:
             self.velocity_y = self.jump_power
             self.on_ground = False
             self._create_dust_particles()
             self.anim_manager.play("jump", force_restart=True)
 
-        # Гравитация
-        if not self.on_ground or self.velocity_y < 0:
+        # Гравитация (всегда применяем если не на земле)
+        if not self.on_ground:
             self.velocity_y += self.gravity
 
         # Применение скорости с проверкой коллизий
         self._move_with_collision(platforms)
 
         # Обновление анимации
+        old_image = self.image
         self.anim_manager.update()
         current_frame = self.anim_manager.get_current_frame()
         if current_frame:
             self.image = current_frame
-            self._update_mask()
+            if self.image != old_image:
+                self._update_masks()
 
-        # Выстрел происходит в середине анимации атаки (кадр 3-4)
+        # Выстрел в середине анимации
         if self.is_attacking and not self.attack_triggered:
             current_anim = self.anim_manager.current_animation
-            if current_anim and current_anim.current_frame == 3:  # середина анимации
+            if current_anim and current_anim.current_frame == 3:
                 self._fire_projectile()
                 self.attack_triggered = True
 
@@ -215,6 +266,7 @@ class Player(pygame.sprite.Sprite):
         self.is_attacking = True
         self.attack_triggered = False
         self.attack_cooldown = PROJECTILE_COOLDOWN
+        self.velocity_x = 0
         self.anim_manager.play("attack", force_restart=True)
 
     def _fire_projectile(self):
@@ -232,77 +284,98 @@ class Player(pygame.sprite.Sprite):
         self._create_shoot_particles()
 
     def _move_with_collision(self, platforms):
-        """Движение с проверкой коллизий по маске"""
-        # Движение по X
+        """Движение с проверкой коллизий"""
+        self._update_hitbox()
+
+        # Движение по X (узкая маска 30px)
         if self.velocity_x != 0:
             self.rect.x += self.velocity_x
+            self._update_hitbox()
             if USE_MASK_COLLISION:
-                self._check_collision_mask(platforms, 'x')
+                self._check_collision_mask_narrow(platforms)
             else:
-                self._check_collision_rect(platforms, 'x')
+                self._check_collision_hitbox(platforms, 'x')
 
         # Движение по Y
-        if self.velocity_y != 0 or not self.on_ground:
-            self.rect.y += self.velocity_y
-            was_on_ground = self.on_ground
-            self.on_ground = False
+        # Сначала применяем гравитацию/скорость
+        self.rect.y += self.velocity_y
+        self._update_hitbox()
 
-            if USE_MASK_COLLISION:
-                self._check_collision_mask(platforms, 'y')
-            else:
-                self._check_collision_rect(platforms, 'y')
+        # Проверяем коллизию с платформами
+        self.on_ground = False
+        if USE_MASK_COLLISION:
+            self._check_collision_mask_full(platforms)
+        else:
+            self._check_collision_hitbox(platforms, 'y')
 
-            if not was_on_ground and self.on_ground:
-                self.rect.y -= self.ground_buffer
-
-    def _check_collision_mask(self, platforms, direction):
-        """Проверка коллизий с использованием масок"""
+    def _check_collision_mask_narrow(self, platforms):
+        """Проверка горизонтальных коллизий с УЗКОЙ маской (30px)"""
         for platform in platforms:
             if not self.rect.colliderect(platform.rect):
                 continue
 
             offset = (
-                platform.rect.x - self.rect.x - self.mask_offset[0],
-                platform.rect.y - self.rect.y - self.mask_offset[1]
+                platform.rect.x - self.rect.x - self.narrow_offset_x,
+                platform.rect.y - self.rect.y - self.narrow_offset_y
+            )
+
+            if platform.mask.overlap(self.narrow_mask, offset):
+                if self.velocity_x > 0:
+                    self.rect.right = platform.rect.left
+                elif self.velocity_x < 0:
+                    self.rect.left = platform.rect.right
+                self.velocity_x = 0
+                self._update_hitbox()
+                return
+
+    def _check_collision_mask_full(self, platforms):
+        """Проверка вертикальных коллизий с ПОЛНОЙ маской"""
+        for platform in platforms:
+            if not self.rect.colliderect(platform.rect):
+                continue
+
+            offset = (
+                platform.rect.x - self.rect.x - self.mask_offset_x,
+                platform.rect.y - self.rect.y - self.mask_offset_y
             )
 
             if platform.mask.overlap(self.mask, offset):
-                if direction == 'x':
-                    if self.velocity_x > 0:
-                        self.rect.right = platform.rect.left
-                    elif self.velocity_x < 0:
-                        self.rect.left = platform.rect.right
-                    self.velocity_x = 0
-                    return
+                if self.velocity_y >= 0:  # Падаем или стоим
+                    # Ставим точно на платформу без буфера
+                    self.rect.bottom = platform.rect.top
+                    self.velocity_y = 0
+                    self.on_ground = True
+                elif self.velocity_y < 0:  # Прыгаем вверх
+                    self.rect.top = platform.rect.bottom
+                    self.velocity_y = 0
+                self._update_hitbox()
+                return
 
-                elif direction == 'y':
-                    if self.velocity_y > 0:
-                        self.rect.bottom = platform.rect.top + self.ground_buffer
-                        self.velocity_y = 0
-                        self.on_ground = True
-                    elif self.velocity_y < 0:
-                        self.rect.top = platform.rect.bottom
-                        self.velocity_y = 0
-                    return
+    def _check_collision_hitbox(self, platforms, direction):
+        """Проверка коллизий с использованием хитбокса 30px"""
+        for platform in platforms:
+            if not self.hitbox.colliderect(platform.rect):
+                continue
 
-    def _check_collision_rect(self, platforms, direction):
-        """Проверка коллизий прямоугольниками (fallback)"""
-        hits = pygame.sprite.spritecollide(self, platforms, False)
-        for platform in hits:
             if direction == 'x':
                 if self.velocity_x > 0:
                     self.rect.right = platform.rect.left
                 elif self.velocity_x < 0:
                     self.rect.left = platform.rect.right
                 self.velocity_x = 0
+                self._update_hitbox()
+                return
+
             elif direction == 'y':
-                if self.velocity_y > 0:
+                if self.velocity_y >= 0:  # Падаем или стоим
                     self.rect.bottom = platform.rect.top
                     self.velocity_y = 0
                     self.on_ground = True
-                elif self.velocity_y < 0:
+                elif self.velocity_y < 0:  # Прыгаем вверх
                     self.rect.top = platform.rect.bottom
                     self.velocity_y = 0
+                self._update_hitbox()
+                return
 
     def _create_shoot_particles(self):
         """Создаёт частицы при выстреле"""
